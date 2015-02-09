45622fdf6f233c8c5a0c66b92240ecc37d2e3860
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-02-09 16:27:17 +0900

    add load data sample

---
+++ sample_load_data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+import numpy as np
+
+
+def load_data(filename):
+    data = []
+    with open(filename, "rb") as f:
+        for row in f.readlines():
+            # firstly, row is like '  1.6191290e+000  4.0366157e+000\r'
+            # row.strip() returns '1.6191290e+000  4.0366157e+000'
+            # row.strip().split() returns ['1.6191290e+000', '4.0366157e+000']
+            # map(float, row.strip().split()) returns [1.619129, 4.0366157]
+            row = map(float, row.strip().split())
+            data.append(row)
+    return np.array(data)
+
+
+def main():
+    data = load_data("data.txt")
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
